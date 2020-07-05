f30907f5dba5e74da98f8e6119b7878b4bc250c3
Novianto <novianto778@gmail.com>
2020-07-05 10:41:09 +0700

    Membuat komponen Header, TodoForm dan Todos

--- src/App.js
+++ src/App.js
@@ -1,45 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Paper from "./components/Paper"
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Paper from "./components/Paper";
+import Header from "./components/Header";
+import TodoForm from "./components/TodoForm";
+import Todos from "./components/Todos";
 
 function App() {
   return (
     <div className="App">
       <Paper>
-      <section className="header">
-            <button className="header-btn main-black-color">Add</button>
-            <h1 className="header-title">Todo Lists</h1>
-            <button className="header-btn main-red-color">Clear</button>
-          </section>
-  
-          <section className="add">
-            <form className="add-form">
-              <input type="text" className="add-input" />
-              <button className="add-btn main-black-color">Add</button>
-            </form>
-          </section>
-  
-          <section className="todos">
-            <div className="todo">
-              <div className="todo-text">Learing React</div>
-            </div>
-            <div className="todo">
-              <div className="todo-text">Learing React</div>
-            </div>
-            <div className="todo">
-              <div className="todo-text">Learing React</div>
-            </div>
-            <div className="todo">
-              <div className="todo-text">Learing React</div>
-            </div>
-            <div className="todo">
-              <div className="todo-text">Learing React</div>
-            </div>
-            <div className="todo">
-              <div className="todo-text">Learing React</div>
-            </div>
-          </section>
+        <Header />
+        <TodoForm />
+        <Todos />
       </Paper>
     </div>
   );
---
+++ src/components/Todos.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function Todos() {
+  return (
+    <section className="todos">
+          <div className="todo">
+            <div className="todo-text">Learing React</div>
+          </div>
+          <div className="todo">
+            <div className="todo-text">Learing React</div>
+          </div>
+          <div className="todo">
+            <div className="todo-text">Learing React</div>
+          </div>
+          <div className="todo">
+            <div className="todo-text">Learing React</div>
+          </div>
+          <div className="todo">
+            <div className="todo-text">Learing React</div>
+          </div>
+          <div className="todo">
+            <div className="todo-text">Learing React</div>
+          </div>
+        </section>
+  )
+}
