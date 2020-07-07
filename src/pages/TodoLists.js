4eaddee3db0025d490dcbcfa150c62842b426d13
Novianto <novianto778@gmail.com>
2020-07-07 19:51:07 +0700

    CSS module todos

--- src/pages/TodoLists.js
+++ src/pages/TodoLists.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Paper from "../components/paper/Paper";
 import Header from "../components/header/Header";
 import TodoForm from "../todoform/TodoForm";
-import Todos from "../components/Todos";
+import Todos from "../todos/Todos";
 
 function TodoLists() {
   const [todos, setTodos] = useState([
--- src/todos/Todos.js
+++ src/todos/Todos.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Todo from "../todo/Todo";
 import PropTypes from "prop-types";
+import styles from "./todos.module.css"
 
 export default function Todos({ todos, completeTodo }) {
   return (
-    <section className="todos">
+    <section className={styles.todos}>
       {todos.length > 0 &&
         todos.map((todo, index) => {
           return (
@@ -18,9 +19,9 @@ export default function Todos({ todos, completeTodo }) {
           );
         })}
       {todos.length === 0 && (
-        <div className="todo-placeholder-text">
+        <div className={styles.todoPlaceholderText}>
           Add Todo by Clicking{" "}
-          <span className="add-button-placeholder-text">Add</span> Button on The
+          <span className={styles.addButtonPlaceholderText}>Add</span> Button on The
           Top Left Corner
         </div>
       )}
