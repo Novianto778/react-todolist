f95d0df96cfc0178f5e877f94f10b8e56f40d95e
Novianto <novianto778@gmail.com>
2020-07-07 19:26:33 +0700

    CSS module paper

---
+++ src/components/paper/Paper.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./paper.module.css";
+
+export default function Paper({ children }) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.frame}>{children}</div>
+    </div>
+  );
+}
+
+Paper.propTypes = {
+  children: PropTypes.oneOfType(
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ),
+};
